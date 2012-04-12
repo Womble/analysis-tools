d522d8dd9fb030dc2ecd9265d0e7bc5157d13f6c
Tom Douglas <pytd@leeds.ac.uk>
2012-04-12 11:13:53 +0100

    added documentation to disc analysis code

--- disc.py
+++ disc.py
@@ -8,6 +8,9 @@ import pyfits as P
 import sys,os
 
 def gauss2 (x, params):
+     """calculate the sum of 2 gaussians and a scalar
+x is a scalar or a numpy array
+params = amplitude1, sigma1, mean1, amplitude2, sigma2, mean2, baseline"""
      a1,s1,m1,a2,s2,m2,c=params
      func1=np.exp(-(x-m1)**2/(2*s1*s1))
      func2=np.exp(-(x-m2)**2/(2*s2*s2))
@@ -18,6 +21,7 @@ def gauss2 (x, params):
      return func1	
 
 def genetic_double_gaussian_fit(im) :
+     "fit double genetic profiles to the each of the spectra in imcube im"
      shape=im.shape
      X=np.arange(float(shape[0]))
      res=np.empty((7,shape[1],shape[2]))
@@ -57,18 +61,20 @@ def genetic_double_gaussian_fit(im) :
              res[:,i,j]=final.pool[0][1:]
      return res
 
-def disc_analysis (path):
+def disc_analysis (path, threshold=0.1):
+    """read in and peform a fitting on the fits file at path containing an imcube and write out
+the results to a new fits file called Xfitted.fits of shape [4,x,y] where the 4 planes of the output are mean of the first and second gaussians"""
     im=cube_convolve(P.getdata(path), 4)
     res=genetic_double_gaussian_fit(im)
     psplit=path.split('.')
     P.writeto(''.join(psplit+['fitted.fits']), res)
-    ans=np.empty([4]+res.shape[1:])
+    ans=np.ones([4]+res.shape[1:])*-1
     shape=res.shape
     for i in xrange(shape[1]):
         for j in xrange(shape[2]):
              ans[2:3,i,j]=res[2:6:3,i,j]
-             if abs(res[0,i,j]) > abs(res[3,i,j])  and abs(res[0,i,j])/abs(res[6,i,j]) > 0.1:
+             if abs(res[0,i,j]) > abs(res[3,i,j])  and abs(res[0,i,j])/abs(res[6,i,j]) > threshold:
                   ans[0,i,j]=res[2,i,j]
-             if abs(res[0,i,j]) <= abs(res[3,i,j]) and abs(res[3,i,j])/abs(res[6,i,j]) > 0.1:
+             if abs(res[0,i,j]) <= abs(res[3,i,j]) and abs(res[3,i,j])/abs(res[6,i,j]) > threshold:
                   ans[1,i,j]=res[5,i,j]
     return ans
